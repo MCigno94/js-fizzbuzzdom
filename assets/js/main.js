16f6a2c52b1c6bd62d79abea38d89682633e7268
MCigno94 <mattiacigno27@gmail.com>
2022-03-28 16:41:24 +0200

    added bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,26 @@
-const elementNumbers = document.querySelector('.numbers');
+const elementContainer = document.querySelector('.container');
+const elementRow = document.createElement('div');
+elementRow.classList.add('row', 'gap-3')
+elementContainer.appendChild(elementRow);
 
 for (let i = 1; i <= 100; i++) {
     const markUp = document.createElement('div');
     console.log(i);
 
     markUp.innerHTML += i;
-    markUp.classList.add('number');
+    markUp.classList.add('number', 'col-1');
 
-    elementNumbers.insertAdjacentElement('beforeend', markUp);
+    elementRow.insertAdjacentElement('beforeend', markUp);
 
     if (i % 3 == 0 && i % 5 == 0) {
-        markUp.innerHTML = "fizzBuzz"
+        markUp.innerHTML = 'fizzBuzz'
+        markUp.style.backgroundColor = 'crimson'
     } else if (i % 5 == 0) {
-        markUp.innerHTML = "buzz"
+        markUp.innerHTML = 'buzz'
+        markUp.classList.add('bg-warning')
     } else if (i % 3 == 0) {
-        markUp.innerHTML = "fizz"
+        markUp.innerHTML = 'fizz'
+        markUp.classList.add('bg-secondary')
     } else {
         markUp.innerHTML = i
     }
