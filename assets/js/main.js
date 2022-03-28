62a5ccf3bcee30e6b2be8c474d21344dae19d98f
MCigno94 <mattiacigno27@gmail.com>
2022-03-28 15:55:00 +0200

    final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,21 @@
 const elementNumbers = document.querySelector('.numbers');
 
 for (let i = 1; i <= 100; i++) {
+    const markUp = document.createElement('div');
     console.log(i);
 
-    const markUp = document.createElement('div');
     markUp.innerHTML += i;
     markUp.classList.add('number');
 
     elementNumbers.insertAdjacentElement('beforeend', markUp);
+
+    if (i % 3 == 0 && i % 5 == 0) {
+        markUp.innerHTML = "fizzBuzz"
+    } else if (i % 5 == 0) {
+        markUp.innerHTML = "buzz"
+    } else if (i % 3 == 0) {
+        markUp.innerHTML = "fizz"
+    } else {
+        markUp.innerHTML = i
+    }
 }
